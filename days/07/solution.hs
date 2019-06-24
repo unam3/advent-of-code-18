03447a7c91f2c9a6cfb2d42d57cbca954f9163d3
Flowers Safety <40meta@gmail.com>
2019-06-25 01:08:54 +0600

    day 7 p2 solution WIP

--- days/07/solution.hs
+++ days/07/solution.hs
@@ -1,7 +1,7 @@
 import System.Environment (getArgs)
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
---import qualified Data.Maybe as Maybe
+import qualified Data.Maybe as Maybe
 
 type Step = Char
 type Dependencies = [Step]
@@ -49,26 +49,90 @@ preprocessInstructionSteps str = foldr makeInstructionStepsToProcess' (makeParti
 
     makeTuple = strToStep_DependencyToStep . words
 
-whichStepToTake :: InstructionStepsToProcess -> Maybe Step
-whichStepToTake instructionStepsToProcess = case Map.keys $ Map.filter (==  "") instructionStepsToProcess of
+whichStepsAvailable :: InstructionStepsToProcess -> Maybe [Step]
+whichStepsAvailable instructionStepsToProcess = case Map.keys $ Map.filter (==  "") instructionStepsToProcess of
     [] -> Nothing
-    step -> Just (head step)
+    steps -> Just steps
+
+whichStepToTake :: Maybe [Step] -> Maybe Step
+whichStepToTake (Just steps) = Just (head steps)
+whichStepToTake _ = Nothing
 
 takeStep :: State -> Step -> State
 takeStep (stepsTaken, instructionStepsToProcess) stepToTake =
     (stepToTake : stepsTaken, updatedInstructionStepsToProcessWithTakenStep) where
         instructionStepsToProcessWithTakenStep = Map.delete stepToTake instructionStepsToProcess
-        updatedInstructionStepsToProcessWithTakenStep = Map.map (List.delete stepToTake) instructionStepsToProcessWithTakenStep
+        updatedInstructionStepsToProcessWithTakenStep =
+            Map.map (List.delete stepToTake) instructionStepsToProcessWithTakenStep
 
 determineOrderOfSteps :: State -> State
-determineOrderOfSteps state@(_, instructionSteps) = case whichStepToTake instructionSteps of
-    Just stepToTake -> determineOrderOfSteps $ takeStep state stepToTake
-    Nothing -> state
+determineOrderOfSteps state@(_, instructionSteps) =
+    case whichStepToTake $ whichStepsAvailable instructionSteps of
+        Just stepToTake -> determineOrderOfSteps $ takeStep state stepToTake
+        Nothing -> state
+
+
+type NumberOfWorkers = Int
+type TimeToCompleteSteps = Int
+type ElapsedTime = Int
+type StepTakesTime = Int
+
+type WorkInProgress = [(ElapsedTime, StepTakesTime, Step)]
+type ConcurrentState = (TimeToCompleteSteps, InstructionStepsToProcess, WorkInProgress)
+
+assignStepsToWorkers :: NumberOfWorkers -> InstructionStepsToProcess -> WorkInProgress -> WorkInProgress
+assignStepsToWorkers freeWorkersCount instructionStepsToProcess workInProgress =
+    if freeWorkersCount > 1 && step /= Nothing
+    -- here we need updated instructionStepsToProcess
+    then assignStepsToWorkers (freeWorkersCount - 1) instructionStepsToProcess workInProgress
+    else newWorkInProgress where
+        newWorkInProgress = ((0, stepTakesTime, Maybe.fromJust step): workInProgress)
+        step = whichStepToTake instructionStepsToProcess
+        --stepTakesTime = 60 + (getStepAdditionaltime step)
+        stepTakesTime = getStepAdditionaltime step
+
+howLongWillItTake :: NumberOfWorkers -> ConcurrentState -> TimeToCompleteSteps
+howLongWillItTake numberOfWorkers state@(_, instructionStepsToProcess, workInProgress) =
+    let freeWorkersCount = numberOfWorkers - (length workInProgress) in
+        case freeWorkersCount > 0 of
+            --True -> howLongWillItTake numberOfWorkers (takeConcurrentStep state stepToTake)
+            True -> assignStepsToWorkers freeWorkersCount instructionStepsToProcess workInProgress
+            False -> state
+
+takeConcurrentStep :: State -> Step -> State
+takeConcurrentStep (stepsTaken, instructionStepsToProcess) stepToTake =
+    (stepToTake : stepsTaken, updatedInstructionStepsToProcessWithTakenStep) where
+        instructionStepsToProcessWithTakenStep = Map.delete stepToTake instructionStepsToProcess
+        updatedInstructionStepsToProcessWithTakenStep =
+            Map.map (List.delete stepToTake) instructionStepsToProcessWithTakenStep
+
+stepToTimeTuples :: [(Char, Int)]
+stepToTimeTuples = List.zip ['A'..'Z'] [1..26]
+
+getStepAdditionaltime :: Char -> Maybe Int
+getStepAdditionaltime step = List.lookup step stepToTimeTuples
+
+{-
+
+Если только один шаг доступен - занимает полное время шага
+Если доступно несколько - загружаем свободных работников
+
+Как считать время для нескольких одновременных задач?
+
+
+Как считать время, если в фоне идет задача?
+
+notion of idle
+-}
+
+
 
 interactWith :: (String -> String) -> FilePath -> FilePath -> IO ()
 interactWith f inputFile outputFile = do
     input <- readFile inputFile
     writeFile outputFile $ f input
+
+--step_amount = zip ['A'..'Z'] [1..26]
 
 main :: IO ()
 main = mainWith solvePuzzle
@@ -78,11 +142,14 @@ main = mainWith solvePuzzle
                 [input, output] -> interactWith f input output
                 _ -> putStrLn "error: exactly two arguments needed"
 
-          solveFirstPuzzlePart input = show . List.reverse . fst . determineOrderOfSteps $ ([], preprocessInstructionSteps input)
+          solveFirstPuzzlePart input =
+            show . List.reverse . fst . determineOrderOfSteps $ ([], preprocessInstructionSteps input)
 
-          -- solveSecondPuzzlePart input =  where
+          --solveSecondPuzzlePart input = howLongWillItTake 2 concurrentState where
+          --  concurrentState = 
 
           solvePuzzle input = "First part solution is: " ++ solveFirstPuzzlePart input ++ "\n"
+          --solvePuzzle input = "Second part solution is: " ++ solveSecondPuzzlePart input
             -- ++ "Second part solution is: " ++ solveSecondPuzzlePart input
 
 {-
@@ -122,4 +189,37 @@ Finally, E is completed.
 So, in this example, the correct order is CABDFE.
 
 In what order should the steps in your instructions be completed?
+
+--- Part Two ---
+
+As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple steps are available, workers should still begin them in alphabetical order.
+
+Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+
+To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the same instructions as above, this is how each second would be spent:
+
+Second   Worker 1   Worker 2   Done
+   0        C          .        
+   1        C          .        
+   2        C          .        
+   3        A          F       C
+   4        B          F       CA
+   5        B          F       CA
+   6        D          F       CAB
+   7        D          F       CAB
+   8        D          F       CAB
+   9        D          .       CABF
+  10        E          .       CABFD
+  11        E          .       CABFD
+  12        E          .       CABFD
+  13        E          .       CABFD
+  14        E          .       CABFD
+  15        .          .       CABFDE
+Each row represents one second of time. The Second column identifies how many seconds have passed as of the beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle). The Done column shows completed steps.
+
+Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers can begin multiple steps simultaneously.
+
+In this example, it would take 15 seconds for two workers to complete these steps.
+
+With 5 workers and the 60+ second step durations described above, how long will it take to complete all of the steps?
 -}
