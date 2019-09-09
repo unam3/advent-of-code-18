4dfaa0bc44c6030516bbd9907e701ab06d09ece9
Flowers Safety <40meta@gmail.com>
2019-09-09 19:29:10 +0600

    day 12 p1 solution

--- days/12/solution.hs
+++ days/12/solution.hs
@@ -2,24 +2,57 @@ import System.Environment (getArgs)
 import qualified Data.List as List
 --import qualified Data.Sequence as Seq
 
---After 20 generations, what is the sum of the numbers of all pots which contain a plant?
-
---newtype State = State String
+type LeftmostPotNumber = Int
 type State = String
-type SpreadToNearbyPotsNote = (String, Char)
---type PotContainPlant = '#'
---type PotNotContainPlant = '.'
+type SpreadPattern = (String, Char)
 
-parseSpreadToNearbyPotsNotes :: String -> SpreadToNearbyPotsNote
-parseSpreadToNearbyPotsNotes string = (pattern, result) where
+parseSpreadPatterns :: String -> SpreadPattern
+parseSpreadPatterns string = (pattern, result) where
     (pattern, rest) = splitAt 5 string
     result = last rest
 
-parseInput :: String -> (State, [SpreadToNearbyPotsNote])
-parseInput input = (initialState, spreadToNearbyPotsNotesList) where
+parseInput :: String -> (State, LeftmostPotNumber, [SpreadPattern])
+parseInput input = (initialState, initialLeftmostPotNumber, spreadPatternsList) where
     (initialStateStringList, rest) = List.splitAt 1 $ lines input
+    initialLeftmostPotNumber = 0
     initialState = last . words $ head initialStateStringList
-    spreadToNearbyPotsNotesList = fmap parseSpreadToNearbyPotsNotes $ drop 1 rest
+    spreadPatternsList = fmap parseSpreadPatterns . filter (List.isSuffixOf "#") $ drop 1 rest
+
+countDots :: State -> Int
+countDots state = 5 - (length $ takeWhile (== '.') state)
+
+makeDotsString :: Int -> State
+makeDotsString dotsToAddNumber = take dotsToAddNumber $ repeat '.'
+
+-- add dots to the left and to the right if less than 5
+fillStateForMatching :: State -> LeftmostPotNumber -> (State, LeftmostPotNumber)
+fillStateForMatching state leftmostPotNumber = (filledState, newLeftmostPotNumber) where
+    filledState = dotsToAddToHead ++ state ++ dotsToAddToTail
+    newLeftmostPotNumber = leftmostPotNumber - dotsToAddToHeadNumber
+    dotsToAddToHeadNumber = countDots $ take 5 state
+    dotsToAddToHead = makeDotsString dotsToAddToHeadNumber
+    dotsToAddToTailNumber = countDots . reverse $ drop (length state - 5) state
+    dotsToAddToTail = makeDotsString dotsToAddToTailNumber
+
+type IsFirstCall = Bool
+
+growGeneration :: (State, LeftmostPotNumber, [SpreadPattern]) -> (State, LeftmostPotNumber, [SpreadPattern])
+growGeneration (state, leftmostPotNumber, spreadPatterns) =
+    (newState, leftmostPotNumber', spreadPatterns) where
+        newState = growGeneration' filledState spreadPatterns True
+        (filledState, leftmostPotNumber') = fillStateForMatching state leftmostPotNumber
+
+growGeneration' :: State -> [SpreadPattern] -> IsFirstCall -> State
+growGeneration' state@(l1:l:_:_:_:_) spreadPatterns isFirstCall =
+    let {
+        restGrown = growGeneration' (drop 1 state) spreadPatterns False;
+        hasSpreadPatternMatch = any ((flip List.isPrefixOf $ state) . fst) spreadPatterns;
+        whatToPut = if hasSpreadPatternMatch then '#' else '.'
+    } in (if isFirstCall then l1:l:whatToPut:[] else [whatToPut]) ++ restGrown
+
+growGeneration' (_:_:r:r1:[]) _ _ = r:r1:[]
+growGeneration' state _ _ = state
+
 
 
 interactWith :: (String -> String) -> FilePath -> FilePath -> IO ()
@@ -38,6 +71,8 @@ main = mainWith solvePuzzle
           solvePuzzle input = "First part solution is: " ++ firstPuzzlePart where
               -- ++ "\n" ++ "Second part solution is: " ++ secondPuzzlePart where
                 firstPuzzlePart =
-                   show (parseInput input)
+                    --show $ parseInput input
+                    show . sum . fmap fst . filter ((/= '.') . snd) $ zip [leftmostPotNumber..] state where
+                        (state, leftmostPotNumber, _) = (List.iterate growGeneration $ parseInput input) !! 20
                 --secondPuzzlePart =
                 --    show .
